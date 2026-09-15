c8ebe87277abf76672b6f02175e8de005d33fe58
Eduard Kachur <glite60@gmail.com>
2026-09-15 22:24:45 +0300

    Increase PWM deadtime value to 2us as per datasheet
    Reduce max current to 30 A nominal

--- shared/f3hw.h
+++ shared/f3hw.h
@@ -21,13 +21,12 @@
 #define PWM_V TIM8->CCR2
 #define PWM_W TIM8->CCR1
 
-//#define PWM_DEADTIME 0
-#define PWM_DEADTIME 60  // 1/144e6 * 58 = 402ns
+#define PWM_DEADTIME 196  // 1/144e6 * 196 = 2us
 #define PWM_RES 4800
 
 #define ABS_MAX_TEMP 110.0
 #define ABS_MAX_VOLT 400.0
-#define ABS_MAX_CURRENT 35.0
+#define ABS_MAX_CURRENT 30.0
 
 //io board
 //#define USB_CONNECT_PIN GPIO_PIN_15
